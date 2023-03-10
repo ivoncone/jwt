b8816eafcf549c650a8e51b1df19206bb53206cd
ivoncone <posdatamexico@gmail.com>
2023-03-09 18:12:01 -0800

    post endpoint events

--- api/events/views.py
+++ api/events/views.py
@@ -8,10 +8,21 @@ from rest_framework.permissions import AllowAny, IsAuthenticated
 from events.models import Events
 from events.serializers import EventSerializer
 
-# Vista de todos los eventos en el sitio web
+# Show the list of all events in website
 class EventsView(APIView):
 	def get(self, request):
 		events = Events.objects.all()
 		serializer = EventSerializer(events, many=True)
 		return Response({'status': 200,
 			'message': serializer.data})
+
+	def post(self, request):
+		data = request.data
+		serializer = EventSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return Response({'status': 201,
+				'message': 'your event has been post',
+				'data': serializer.data})
+		return Response({'status': 400, 'message': 'algun dato falta'})
+
