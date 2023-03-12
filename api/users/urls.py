351dade972e1fd5770068feddcc71789b67eca09
ivoncone <posdatamexico@gmail.com>
2023-03-11 16:01:57 -0800

    user login url

--- api/users/urls.py
+++ api/users/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
 from users import views
-from users.views import UserSignUpView
+from users.views import UserSignUpView, LoginView
 
 urlpatterns = [
-	path('signup/', views.UserSignUpView.as_view())
+	path('signup/', views.UserSignUpView.as_view()),
+	path('login/', views.LoginView.as_view())
 ]
